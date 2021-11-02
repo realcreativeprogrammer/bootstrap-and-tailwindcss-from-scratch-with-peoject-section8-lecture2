e5ae22ec0591fe482271e671d378a3e63a6bd6c3
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2021-11-02 19:44:32 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [
+    require("tailwindcss-font-variant-numeric"),
+    require('@tailwindcss/forms'),
+
+  ],
+}
